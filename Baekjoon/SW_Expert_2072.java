a9869f3145ba6faad055dec1df9ff7d726e7fa84
Han5ung <hsh5349@naver.com>
2021-12-31 00:22:01 +0900

    2nd commit

---
+++ Baekjoon/SW_Expert_2072.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+import java.io.FileInputStream;
+
+public class SW_Expert_2072 {
+	public static int t, num1;
+	public static int num = 0;
+
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		t = sc.nextInt();
+
+		for (int i = 1; i <= t; i++) {
+			for (int j = 0; j < 10; j++) {
+				num1 = sc.nextInt();
+				sum(num1);
+			}
+			System.out.println("#" + i + " " + num);
+			num = 0;
+		}
+	}
+
+	public static void sum(int a) {
+
+		if (a % 2 != 0) {
+			num += a;
+
+		}
+	}
+
+}
